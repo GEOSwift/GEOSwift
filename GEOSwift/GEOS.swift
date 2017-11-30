5fe1490e2152df7dc105544f15ce6a047db89a8a
Virgilio Favero Neto <virgiliofavero@gmail.com>
2017-12-01 10:10:38 +1100

    Using GEOS_init_r

--- GEOSwift/GEOS.swift
+++ GEOSwift/GEOS.swift
@@ -10,16 +10,19 @@ import Foundation
 typealias GEOSCallbackFunction = @convention(c) (UnsafeMutableRawPointer) -> Void
 
 let swiftCallback : GEOSCallbackFunction = {
-	#if DEBUG
-		guard let string = String(validatingUTF8: $0.assumingMemoryBound(to: CChar.self)) else { return }
-		print("GEOSwift # " + string + ".")
-	#endif
+    guard let string = String(validatingUTF8: $0.assumingMemoryBound(to: CChar.self)) else { return }
+    print("GEOSwift # " + string + ".")
 }
 
 var GEOS_HANDLE: OpaquePointer = {
-//    return initGEOSWrapper_r();
-    return initGEOS_r(unsafeBitCast(swiftCallback, to: GEOSMessageHandler.self),
-        unsafeBitCast(swiftCallback, to: GEOSMessageHandler.self))
+#if DEBUG
+    guard let handle = GEOS_init_r() else { preconditionFailure() }
+    GEOSContext_setNoticeMessageHandler_r(handle, unsafeBitCast(swiftCallback, to: GEOSMessageHandler_r.self), nil)
+    GEOSContext_setErrorMessageHandler_r(handle, unsafeBitCast(swiftCallback, to: GEOSMessageHandler_r.self), nil)
+    return handle
+#else
+    return GEOS_init_r()
+#endif
 }()
 
 public typealias CoordinateDegrees = Double
