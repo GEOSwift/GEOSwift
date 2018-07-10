ad75d379bcc42fc750a1dd242efec627b0afdcdb
Andrew Hershberger <andrew.hershberger@transloc-inc.com>
2018-07-10 09:59:37 -0400

    Add files to framework target

--- GEOSwift/MiscFunctions.swift
+++ GEOSwift/MiscFunctions.swift
@@ -7,18 +7,12 @@
 
 import Foundation
 
-/**
- Misc functions
- */
-
 public extension Geometry {
-
     /// - returns: The distance between the two geometries, expressed in the SRID of the first
     func distance(geometry: Geometry) -> Double {
         var dist: Double = 0
-
-        let result = GEOSDistance_r(GEOS_HANDLE, self.geometry, geometry.geometry, &dist)
-        assert (result == 1)
+        let result = GEOSDistance_r(GEOS_HANDLE, storage.GEOSGeom, geometry.storage.GEOSGeom, &dist)
+        assert(result == 1)
         return dist
     }
 }
