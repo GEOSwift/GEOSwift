3854cb0ce18470be1f4a81a210ee12955c74ec7b
yushiro otsuka <yushiro.otsuka@optim.co.jp>
2021-10-14 16:56:36 +0900

    Add PolygonZ+Codable.swift

---
+++ GEOSwift/Codable/PolygonZ+Codable.swift
@@ -0,0 +1,27 @@
+extension PolygonZ.LinearRing {
+    var coordinates: [[Double]] {
+        points.map { $0.coordinates }
+    }
+
+    init(coordinates: [[Double]]) throws {
+        try self.init(points: coordinates.map(PointZ.init))
+    }
+}
+
+// for internal use only; GeoJSON encoding & decoding helpers
+extension PolygonZ: CodableGeometry {
+    static let geoJSONType = GeoJSONType.polygon
+
+    var coordinates: [[[Double]]] {
+        let allRings = [exterior] + holes
+        return allRings.map { $0.coordinates }
+    }
+
+    init(coordinates: [[[Double]]]) throws {
+        let rings = try coordinates.map(LinearRing.init)
+        guard rings.count >= 1 else {
+            throw GEOSwiftError.tooFewRings
+        }
+        self.init(exterior: rings[0], holes: Array(rings[1..<rings.count]))
+    }
+}
