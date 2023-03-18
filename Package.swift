6d77c1f11022922f21d55df121a85c16807bf7ef
Andrew Hershberger <andrew.d.hershberger@gmail.com>
2023-03-18 14:10:56 -0400

    Update to GEOSwift/geos 8.1.0 (geos 3.11.2)

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "GEOSwift", targets: ["GEOSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/GEOSwift/geos.git", from: "8.0.0")
+        .package(url: "https://github.com/GEOSwift/geos.git", from: "8.1.0")
     ],
     targets: [
         .target(
