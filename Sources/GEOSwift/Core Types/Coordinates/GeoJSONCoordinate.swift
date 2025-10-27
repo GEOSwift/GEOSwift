73311274d619d5fc2934672a74ff77b2541d4456
Scott Hoyt <scottrhoyt@gmail.com>
2025-10-27 14:36:09 -0700

    Add documentation to GeoJSONCoordinate.

--- Sources/GEOSwift/Core Types/Coordinates/GeoJSONCoordinate.swift
+++ Sources/GEOSwift/Core Types/Coordinates/GeoJSONCoordinate.swift
@@ -1,3 +1,6 @@
 import Foundation
 
+/// A marker protocol indicating that a coordinate type can be encoded/decoded as GeoJSON.
+///
+/// Conforming types: ``XY``, ``XYZ``
 public protocol GeoJSONCoordinate: Codable {}
