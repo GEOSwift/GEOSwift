14c4f3af831972b970b1c291e5b3e2e814f51bd0
Scott Hoyt <scottrhoyt@gmail.com>
2025-10-27 14:34:57 -0700

    Add documentation to GeoJSON.

--- Sources/GEOSwift/Core Types/GeoJSON.swift
+++ Sources/GEOSwift/Core Types/GeoJSON.swift
@@ -1,5 +1,11 @@
+/// A top-level GeoJSON object that can represent a ``FeatureCollection``, ``Feature``, or ``Geometry``.
+///
+/// The underlying ``CoordinateType`` (e.g. ``XY``, ``XYZ``) determines the dimensionality.
 public enum GeoJSON<C: CoordinateType>: Hashable, Sendable {
+    /// A ``FeatureCollection`` GeoJSON object
     case featureCollection(FeatureCollection<C>)
+    /// A ``Feature`` GeoJSON object
     case feature(Feature<C>)
+    /// A ``Geometry`` GeoJSON object
     case geometry(Geometry<C>)
 }
