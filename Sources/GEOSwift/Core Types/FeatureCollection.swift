dbe0a374b9b07e38663538185f15612d3461f2f7
Scott Hoyt <scottrhoyt@gmail.com>
2025-10-27 14:34:35 -0700

    Add documentation to FeatureCollection.

--- Sources/GEOSwift/Core Types/FeatureCollection.swift
+++ Sources/GEOSwift/Core Types/FeatureCollection.swift
@@ -1,6 +1,13 @@
+/// A GeoJSON FeatureCollection representing a collection of zero or more ``Feature``s.
+///
+/// The underlying ``CoordinateType`` (e.g. ``XY``, ``XYZ``) determines the dimensionality of the geometries.
 public struct FeatureCollection<C: CoordinateType>: Hashable, Sendable {
+    /// The features in the `FeatureCollection`
     public var features: [Feature<C>]
 
+    /// Initialize a `FeatureCollection` from the given features.
+    /// - parameters:
+    ///   - features: An array of features.
     public init(features: [Feature<C>]) {
         self.features = features
     }
