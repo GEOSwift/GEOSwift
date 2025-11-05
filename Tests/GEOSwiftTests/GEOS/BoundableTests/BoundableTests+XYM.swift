9f4fbc7bdbf4080969c803a88efeb38597062eeb
Scott Hoyt <scottrhoyt@gmail.com>
2025-11-05 14:43:36 -0800

    Use fixtures for Boundable tests.

--- Tests/GEOSwiftTests/GEOS/BoundableTests/BoundableTests+XYM.swift
+++ Tests/GEOSwiftTests/GEOS/BoundableTests/BoundableTests+XYM.swift
@@ -2,52 +2,14 @@ import XCTest
 import GEOSwift
 
 final class BoundableTests_XYM: GEOSTestCase_XYM {
-    let boundables: [any Boundable<XYM>] = [
-        Point(x: 1, y: 2, m: 0),
-        MultiPoint(points: [Point(x: 1, y: 2, m: 0), Point(x: 3, y: 4, m: 1)]),
-        try! LineString(coordinates: [XYM(1, 2, 0), XYM(3, 4, 1)]),
-        MultiLineString(lineStrings: [
-            try! LineString(coordinates: [XYM(1, 2, 0), XYM(3, 4, 1)]),
-            try! LineString(coordinates: [XYM(5, 6, 2), XYM(7, 8, 3)])
-        ]),
-        try! Polygon.LinearRing(coordinates: [
-            XYM(1, 1, 0),
-            XYM(1, -1, 0),
-            XYM(-1, -1, 0),
-            XYM(-1, 1, 0),
-            XYM(1, 1, 1)]),
-        Polygon(exterior: try! Polygon.LinearRing(coordinates: [
-            XYM(2, 2, 0),
-            XYM(-2, 2, 0),
-            XYM(-2, -2, 0),
-            XYM(2, -2, 0),
-            XYM(2, 2, 1)]),
-            holes: [try! Polygon.LinearRing(coordinates: [
-                XYM(1, 1, 0),
-                XYM(1, -1, 0),
-                XYM(-1, -1, 0),
-                XYM(-1, 1, 0),
-                XYM(1, 1, 1)])]),
-        MultiPolygon(polygons: [
-            Polygon(exterior: try! Polygon.LinearRing(coordinates: [
-                XYM(2, 2, 0),
-                XYM(-2, 2, 0),
-                XYM(-2, -2, 0),
-                XYM(2, -2, 0),
-                XYM(2, 2, 1)]),
-                holes: [try! Polygon.LinearRing(coordinates: [
-                    XYM(1, 1, 0),
-                    XYM(1, -1, 0),
-                    XYM(-1, -1, 0),
-                    XYM(-1, 1, 0),
-                    XYM(1, 1, 1)])]),
-            Polygon(exterior: try! Polygon.LinearRing(coordinates: [
-                XYM(7, 2, 0),
-                XYM(3, 2, 0),
-                XYM(3, -2, 0),
-                XYM(7, -2, 0),
-                XYM(7, 2, 1)]))
-        ])]
+    lazy var boundables: [any Boundable<XYM>] = [
+        point1,
+        multiPoint,
+        lineString1,
+        multiLineString,
+        linearRingHole1,
+        polygonWithHole,
+        multiPolygon]
 
     func testBoundaryLine() {
         let line = try! LineString(coordinates: [XYM(0, 0, 0), XYM(1, 0, 1)])
--- Tests/GEOSwiftTests/GEOS/BoundableTests/BoundableTests+XYZ.swift
+++ Tests/GEOSwiftTests/GEOS/BoundableTests/BoundableTests+XYZ.swift
@@ -2,52 +2,14 @@ import XCTest
 import GEOSwift
 
 final class BoundableTests_XYZ: GEOSTestCase_XYZ {
-    let boundables: [any Boundable<XYZ>] = [
-        Point(x: 1, y: 2, z: 0),
-        MultiPoint(points: [Point(x: 1, y: 2, z: 0), Point(x: 3, y: 4, z: 1)]),
-        try! LineString(coordinates: [XYZ(1, 2, 0), XYZ(3, 4, 1)]),
-        MultiLineString(lineStrings: [
-            try! LineString(coordinates: [XYZ(1, 2, 0), XYZ(3, 4, 1)]),
-            try! LineString(coordinates: [XYZ(5, 6, 2), XYZ(7, 8, 3)])
-        ]),
-        try! Polygon.LinearRing(coordinates: [
-            XYZ(1, 1, 0),
-            XYZ(1, -1, 0),
-            XYZ(-1, -1, 0),
-            XYZ(-1, 1, 0),
-            XYZ(1, 1, 1)]),
-        Polygon(exterior: try! Polygon.LinearRing(coordinates: [
-            XYZ(2, 2, 0),
-            XYZ(-2, 2, 0),
-            XYZ(-2, -2, 0),
-            XYZ(2, -2, 0),
-            XYZ(2, 2, 1)]),
-            holes: [try! Polygon.LinearRing(coordinates: [
-                XYZ(1, 1, 0),
-                XYZ(1, -1, 0),
-                XYZ(-1, -1, 0),
-                XYZ(-1, 1, 0),
-                XYZ(1, 1, 1)])]),
-        MultiPolygon(polygons: [
-            Polygon(exterior: try! Polygon.LinearRing(coordinates: [
-                XYZ(2, 2, 0),
-                XYZ(-2, 2, 0),
-                XYZ(-2, -2, 0),
-                XYZ(2, -2, 0),
-                XYZ(2, 2, 1)]),
-                holes: [try! Polygon.LinearRing(coordinates: [
-                    XYZ(1, 1, 0),
-                    XYZ(1, -1, 0),
-                    XYZ(-1, -1, 0),
-                    XYZ(-1, 1, 0),
-                    XYZ(1, 1, 1)])]),
-            Polygon(exterior: try! Polygon.LinearRing(coordinates: [
-                XYZ(7, 2, 0),
-                XYZ(3, 2, 0),
-                XYZ(3, -2, 0),
-                XYZ(7, -2, 0),
-                XYZ(7, 2, 1)]))
-        ])]
+    lazy var boundables: [any Boundable<XYZ>] = [
+        point1,
+        multiPoint,
+        lineString1,
+        multiLineString,
+        linearRingHole1,
+        polygonWithHole,
+        multiPolygon]
 
     func testBoundaryLine() {
         let line = try! LineString(coordinates: [XYZ(0, 0, 0), XYZ(1, 0, 1)])
