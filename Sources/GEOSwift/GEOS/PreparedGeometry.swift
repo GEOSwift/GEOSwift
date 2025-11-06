39c23676f46e9dc18067637dec32acf2a6cb894b
Scott Hoyt <scottrhoyt@gmail.com>
2025-11-05 16:46:21 -0800

    Fix swiftlint violations.

--- Sources/GEOSwift/GEOS/PreparedGeometry.swift
+++ Sources/GEOSwift/GEOS/PreparedGeometry.swift
@@ -16,7 +16,7 @@ public final class PreparedGeometry<C: CoordinateType> {
     init(context: GEOSContext, base: GEOSObject) throws {
         self.context = context
         self.base = base
-        
+
         guard let pointer = GEOSPrepare_r(context.handle, base.pointer) else {
             throw GEOSError.libraryError(errorMessages: context.errors)
         }
@@ -37,13 +37,13 @@ public final class PreparedGeometry<C: CoordinateType> {
     /// - Throws: `Error` if the containment test fails.
     public func contains(_ geometry: any GeometryConvertible<C>) throws -> Bool {
         let geosObject = try geometry.geometry.geosObject(with: context)
-        
+
         // returns 1 on true, 0 on false, 2 on exception
         let result = GEOSPreparedContains_r(context.handle, pointer, geosObject.pointer)
         guard result != 2 else {
             throw GEOSError.libraryError(errorMessages: context.errors)
         }
-        
+
         return result == 1
     }
 
@@ -57,13 +57,13 @@ public final class PreparedGeometry<C: CoordinateType> {
     /// - Throws: `Error` if the containment test fails.
     public func containsProperly(_ geometry: any GeometryConvertible<C>) throws -> Bool {
         let geosObject = try geometry.geometry.geosObject(with: context)
-        
+
         // returns 1 on true, 0 on false, 2 on exception
         let result = GEOSPreparedContainsProperly_r(context.handle, pointer, geosObject.pointer)
         guard result != 2 else {
             throw GEOSError.libraryError(errorMessages: context.errors)
         }
-        
+
         return result == 1
     }
 }
